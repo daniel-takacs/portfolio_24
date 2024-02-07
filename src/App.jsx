54bcb63ca02ec24e605e5eee69c6660a2f17082c
dta <daniel.takacs@sofiadigital.com>
2024-02-07 10:49:37 +0200

    Add components, NavBar and pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import { Home, About, Projects, Contact } from './pages'
+
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <main className="bg-main">
+    <Router>
+        <NavBar />
+        <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/about" element={<About />}/>
+            <Route path="/projects" element={<Projects />}/>
+            <Route path="/contact" element={<Contact />}/>
+        </Routes>
+    </Router>
+  </main>;
 }
 
-export default App
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import { NavLink } from "react-router-dom";
+import { logo } from "../assets/images/index";
+
+const NavBar = () => {
+  return (
+    <header className="header">
+      <NavLink to="/" className="w-8 h-8 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
+        <img src={logo} alt="" />
+      </NavLink>
+      <nav className="flex text-lg gap-7 font-medium">
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/projects">Projects</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
